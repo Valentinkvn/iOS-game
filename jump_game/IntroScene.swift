dc0291952360dc8124a2f42a86e4d83c68d2152c
Valentin <valentin@Valentins-Mac.local>
2020-04-15 04:24:59 +0300

    Add functionalities

--- jump_game/IntroScene.swift
+++ jump_game/IntroScene.swift
@@ -11,24 +11,45 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var sound: Bool = true
+    
+    var background = SKSpriteNode()
+    var gameLogo = SKSpriteNode()
+    var playButton = SKSpriteNode()
+    var soundButton = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
         self.initMenu()
     }
     
-
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let playButton = self.childNode(withName: "playButton")
+
+        let backgroundMusic = self.childNode(withName: "backgroundMusic") as! SKAudioNode
         
         for touch: AnyObject in touches{
             let pointOfTouch = touch.location(in: self)
-            if (playButton?.contains(pointOfTouch))!{
+            // the play button was pressed
+            if (playButton.contains(pointOfTouch)){
                 let fadeOut = SKAction.fadeOut(withDuration: 1.0)
-                playButton?.run(fadeOut, completion: {
+                playButton.run(fadeOut, completion: {
                     let doors = SKTransition.doorway(withDuration: 1.5)
                     let jumperScene = JumperScene(fileNamed: "JumperScene")
                     self.view?.presentScene(jumperScene!, transition: doors)
                 })
+            }
+            // the sound button was pressed
+            if (soundButton.contains(pointOfTouch)){
+                if (sound == true){
+                    soundButton.texture = SKTexture(imageNamed: "mute")
+                    backgroundMusic.run(SKAction.stop())
+                    sound = false
+                }
+                else{
+                    soundButton.texture = SKTexture(imageNamed: "sound")
+                    backgroundMusic.run(SKAction.play())
+                    sound = true
+                }
+                
             }
         }
         
@@ -42,8 +63,9 @@ class GameScene: SKScene {
     private func initMenu() {
 //        let height = self.size.height
         let width = self.size.width
+        let height = self.size.height
         
-        let background = SKSpriteNode(imageNamed: "rainforest")
+        background = SKSpriteNode(imageNamed: "rainforest")
         background.name = "backgroundNode"
         background.size = self.size
         background.position = CGPoint(x: 0, y: 0)
@@ -51,7 +73,7 @@ class GameScene: SKScene {
         self.addChild(background)
         
         // set the game logo properties
-        let gameLogo = SKSpriteNode(imageNamed: "logo")
+        gameLogo = SKSpriteNode(imageNamed: "logo")
         gameLogo.name = "gameLogo"
         gameLogo.zPosition = 1
         gameLogo.position = CGPoint(x: 0, y: 250)
@@ -59,17 +81,25 @@ class GameScene: SKScene {
         self.addChild(gameLogo)
 
         //Create play button
-        let playButton = SKSpriteNode(imageNamed: "play")
+        playButton = SKSpriteNode(imageNamed: "play")
         playButton.name = "playButton"
         playButton.zPosition = 1
         playButton.position = CGPoint(x: 0, y: -width/6 )
-//        playButton.fillColor = SKColor.cyan
-        
-        // set the scale to 0 to have a starting poit for scaling transform
+        // set the scale to 0 to have a starting point for scaling transform
         playButton.setScale(0)
-
         playButton.run(SKAction.scale(to: 0.6, duration: 1.0))
         self.addChild(playButton)
+        
+        soundButton = SKSpriteNode(imageNamed: "sound")
+        soundButton.name = "soundButton"
+        soundButton.zPosition = 1
+        soundButton.position = CGPoint(x: -2*height/3, y: -width/6)
+        self.addChild(soundButton)
+        
+        let soundUrl = Bundle.main.url(forResource: "Jungle", withExtension: "mp3")!
+        let backgroundMusic = SKAudioNode(url: soundUrl)
+        backgroundMusic.name = "backgroundMusic"
+        self.addChild(backgroundMusic)
         
         
     }
--- jump_game/JumperScene.swift
+++ jump_game/JumperScene.swift
@@ -3,35 +3,18 @@ import SpriteKit
 
 class JumperScene: SKScene {
     
-    var jumperAnimation = [SKTexture]()
+    var jumperRunning = [SKTexture]()
+    var jumperIdle = [SKTexture]()
+    var jumperJump = [SKTexture]()
+    var jumperHit = [SKTexture]()
+    var jumperGround = [SKTexture]()
+    
+    var background = SKSpriteNode()
+    var ground = SKSpriteNode()
+    
+    
     override func didMove(to view: SKView) {
         self.initJumperScene()
-    }
-    
-    func initJumperScene() {
-        let background = SKSpriteNode(imageNamed: "rainforest")
-        background.size = self.size
-        print(self.size)
-        background.position = CGPoint(x: 0, y: 0)
-        background.zPosition = 0
-        background.name = "backgroundNode"
-        self.addChild(background)
-        
-        let ground = SKSpriteNode(imageNamed: "ground")
-        ground.size.width = self.size.width
-        ground.size.height = self.size.height/10
-        ground.position = CGPoint(x: 0, y: -self.size.height/2 + 15)
-        ground.zPosition = 1
-        ground.name = "groundNode"
-        self.addChild(ground)
-        
-        let jumperAtlas = SKTextureAtlas(named: "running")
-        print(jumperAtlas.textureNames.count)
-        
-        for index in 1...jumperAtlas.textureNames.count{
-            let imgName = String(format: "frame-running-%01d", index)
-            jumperAnimation += [jumperAtlas.textureNamed(imgName)]
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -41,13 +24,55 @@ class JumperScene: SKScene {
             let pointOfTouch = touch.location(in: self)
             if(pointOfTouch.x > 0) {
                 if (jumperNode != nil){
-                    let animation = SKAction.animate(with: jumperAnimation, timePerFrame:0.1)
+                    let animation = SKAction.animate(with: jumperRunning, timePerFrame:0.1)
                     jumperNode?.run(animation)
+                    let groundAnimation = SKAction.animate(with: jumperGround, timePerFrame:0.4)
+                    ground.run(groundAnimation)
+                    
                 }
             }
         }
-        
     }
     
+    func initJumperScene() {
+        background = SKSpriteNode(imageNamed: "rainforest")
+        background.size = self.size
+        print(self.size)
+        background.position = CGPoint(x: 0, y: 0)
+        background.zPosition = 0
+        background.name = "backgroundNode"
+        self.addChild(background)
+        
+        ground = SKSpriteNode(imageNamed: "ground-init")
+        ground.size.width = self.size.width
+        ground.size.height = self.size.height/10
+        ground.position = CGPoint(x: 0, y: -self.size.height/2 + 15)
+        ground.zPosition = 1
+        ground.name = "groundNode"
+        self.addChild(ground)
+        
+        let runningAtlas = SKTextureAtlas(named: "running")
+        let idleAtlas = SKTextureAtlas(named: "idle")
+        let hitAtlas = SKTextureAtlas(named: "gotHit")
+        let jumpAtlas = SKTextureAtlas(named: "jump")
+        let groundAtlas = SKTextureAtlas(named: "ground")
+        
+        jumperRunning = grabAtlas(dataAtlas: runningAtlas, label: "running")
+        jumperIdle = grabAtlas(dataAtlas: idleAtlas, label: "idle")
+        jumperHit = grabAtlas(dataAtlas: hitAtlas, label: "gotHit")
+        jumperJump = grabAtlas(dataAtlas: jumpAtlas, label: "jump")
+        jumperGround = grabAtlas(dataAtlas: groundAtlas, label: "ground")
+    }
+    
+    func grabAtlas(dataAtlas: SKTextureAtlas, label: String) -> [SKTexture]{
+        var atlas = [SKTexture]()
+        print(dataAtlas.textureNames.count)
+        for index in 1...dataAtlas.textureNames.count{
+            let path = "frame-" + label + "-%01d"
+            let imgName = String(format: path, index)
+            atlas += [dataAtlas.textureNamed(imgName)]
+        }
+        return atlas
+    }
     
 }
